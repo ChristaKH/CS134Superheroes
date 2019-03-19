dd27347561ce9c79c2443aa76bb9144e6e2b5998
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-03-19 12:25:30 -0800

    Add private variables to main activity

--- app/src/main/java/chatch/cs134/cs134superheroes/MainActivity.java
+++ app/src/main/java/chatch/cs134/cs134superheroes/MainActivity.java
@@ -1,9 +1,33 @@
 package chatch.cs134.cs134superheroes;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
+import java.util.List;
+
+import chatch.cs134.cs134superheroes.Model.Superhero;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView questionNumberTextView;
+    private ImageView personImageView;
+    private TextView guessSubjectTextView;
+    private Button[] mButtons = new Button[4];
+
+    private List<Superhero> allSuperheroesList;  // all the heroes loaded from JSON
+    private List<Superhero> mQuizSuperheroList; // superheroes in current quiz (just 10 of them)
+    private Superhero mCorrectSuperhero; // correct superhero for the current question
+    private int mTotalGuesses; // number of total guesses made
+    private int mCorrectGuesses; // number of correct guesses
+    private SecureRandom rng; // used to randomize the quiz
+    private Handler handler; // used to delay loading next superhero
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
