b054ed67ffc86ed03bc07e76b22e10697ce38dfb
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-03-25 22:21:20 -0800

    Add javadoc comments for Setting class

--- app/src/main/java/chatch/cs134/cs134superheroes/Model/Settings.java
+++ app/src/main/java/chatch/cs134/cs134superheroes/Model/Settings.java
@@ -1,30 +1,62 @@
+/**
+ * The Settings class keeps track of the types of settings
+ * that are available to the user. Setting class includes
+ * the setting type and the setting summary.
+ */
 package chatch.cs134.cs134superheroes.Model;
 
 public class Settings {
     private String mSettingType;
     private String mSettingSummary;
 
+    /**
+     * Full constructor that sets the setting type and the
+     * setting summary.
+     * @param settingType Type of setting that user can change
+     * @param settingSummary String that is the summary of the setting
+     *                       being adjusted.
+     */
     public Settings(String settingType, String settingSummary) {
         mSettingType = settingType;
         mSettingSummary = settingSummary;
     }
 
+    /**
+     * Method that returns the type of setting available
+     * @return the type of setting
+     */
     public String getSettingType() {
         return mSettingType;
     }
 
+    /**
+     * Changes the type of setting being adjusted.
+     * @param settingType   The new type of setting
+     */
     public void setSettingType(String settingType) {
         mSettingType = settingType;
     }
 
+    /**
+     * Method that returns the String summary of the setting available.
+     * @return  Setting summary
+     */
     public String getSettingSummary() {
         return mSettingSummary;
     }
 
+    /**
+     * Method that sets the summary of the current setting
+     * @param settingSummary    New summary for the current setting
+     */
     public void setSettingSummary(String settingSummary) {
         mSettingSummary = settingSummary;
     }
 
+    /**
+     * Method that returns the String summary of the Setting
+     * @return  String summary of setting
+     */
     @Override
     public String toString() {
         return "Settings{" +
